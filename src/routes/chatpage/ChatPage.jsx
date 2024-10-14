2845ed9dab989bdf796d8213d00c78221e4d52ad
kquewvin <117505337+kquewvin@users.noreply.github.com>
2024-10-14 19:36:02 -0400

    react image upload and chat page design

--- src/routes/chatpage/ChatPage.jsx
+++ src/routes/chatpage/ChatPage.jsx
@@ -1,9 +1,55 @@
-import './chatpage.css'
+import "./chatpage.css";
+import NewPrompt from "../../components/newPrompt/newPromp";
 
 const ChatPage = () => {
-    return (
-        <div className='chatpage'>ChatPage</div>
-    )
-}
+	return (
+		<div className="chatPage">
+			<div className="wrapper">
+				<div className="chat">
+					<div className="message">TEST</div>
+					<div className="message user">
+						TEST FROM USER Lorem ipsum, dolor sit amet consectetur adipisicing
+						elit. Fuga eveniet ut ducimus molestias atque. Perferendis,
+						mollitia? Ut ea pariatur odit! Quo veritatis reiciendis magnam
+						temporibus reprehenderit nisi expedita quod pariatur.
+					</div>
+					<div className="message ">TEST</div>
+					<div className="message user">TEST FROM USER</div>
+					<div className="message">TEST</div>
+					<div className="message user">TEST FROM USER</div>
+					<div className="message ">TEST</div>
+					<div className="message user">TEST FROM USER</div>
+					<div className="message">TEST</div>
+					<div className="message user">TEST FROM USER</div>
+					<div className="message ">TEST</div>
+					<div className="message user">TEST FROM USER</div>
+					<div className="message">TEST</div>
+					<div className="message user">TEST FROM USER</div>
+					<div className="message ">TEST</div>
+					<div className="message user">TEST FROM USER</div>
+					<div className="message">TEST</div>
+					<div className="message user">TEST FROM USER</div>
+					<div className="message ">TEST</div>
+					<div className="message user">TEST FROM USER</div>
+					<div className="message ">TEST</div>
+					<div className="message user">TEST FROM USER</div>
+					<div className="message ">TEST</div>
+					<div className="message user">TEST FROM USER</div>
+					<div className="message ">TEST</div>
+					<div className="message user">TEST FROM USER</div>
+					<div className="message ">TEST</div>
+					<div className="message user">TEST FROM USER</div>
+					<div className="message ">TEST</div>
+					<div className="message user">TEST FROM USER</div>
+					<div className="message ">TEST</div>
+					<div className="message user">TEST FROM USER</div>
+					<div className="message ">TEST</div>
+					<div className="message user">TEST FROM USER</div>
+					<NewPrompt />
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default ChatPage;
