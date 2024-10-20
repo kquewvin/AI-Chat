806a46985fe4180c5d4101645a8986a57465cf63
kquewvin <117505337+kquewvin@users.noreply.github.com>
2024-10-20 03:13:24 -0400

    added gemini ai api. can now converse with ai in chat page

--- client/src/components/newPrompt/newPrompt.jsx
+++ client/src/components/newPrompt/newPrompt.jsx
@@ -2,8 +2,11 @@ import { useEffect, useRef, useState } from "react";
 import "./newPrompt.css";
 import Upload from "../upload/Upload";
 import { IKImage } from "imagekitio-react";
+import model from "../../lib/gemini";
 
 const NewPrompt = () => {
+	const [question, setQuestion] = useState("");
+	const [answer, setAnswer] = useState("");
 	const [img, setImg] = useState({
 		isLoading: false,
 		error: "",
@@ -14,6 +17,23 @@ const NewPrompt = () => {
 	useEffect(() => {
 		endRef.current.scrollIntoView({ behavior: "smooth" });
 	}, []);
+
+	const add = async (text) => {
+		setQuestion(text)
+
+		const result = await model.generateContent(text);
+		const response = await result.response;
+		setAnswer(response.text())
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		const text = e.target.text.value;
+		if (!text) return;
+
+		add(text);
+	};
 
 	return (
 		<>
@@ -26,11 +46,13 @@ const NewPrompt = () => {
 					transformation={[{ width: 380 }]}
 				/>
 			)}
+			{question && <div className="message user">{question}</div>}
+			{answer && <div className="message">{answer}</div>}
 			<div className="endChat" ref={endRef}></div>
-			<form className="newForm">
+			<form className="newForm" onSubmit={handleSubmit}>
 				<Upload setImg={setImg} />
 				<input id="file" type="file" multiple={false} hidden />
-				<input type="text" placeholder="Ask me anything" />
+				<input type="text" name="text" placeholder="Ask me anything" />
 				<button>
 					<img src="/arrow.png" alt="" />
 				</button>
